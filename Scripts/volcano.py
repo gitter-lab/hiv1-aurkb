6a9303fc7c99f106c98ef88a037f6e7744697923
Anthony Gitter <gitter@biostat.wisc.edu>
2023-03-31 13:31:43 -0500

    Generate 5 and 60 min volcano plots Use latest version of volcano plot package from fork

--- Scripts/volcano.py
+++ Scripts/volcano.py
@@ -2,6 +2,7 @@
 Generate volcano plots from wild type proteomic changes
 """
 import argparse
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
@@ -10,15 +11,15 @@ from sanbomics.plots import volcano
 
 def main():
     parser = argparse.ArgumentParser(description="Generate and save volcano plots. The output filenames are derived "
-                                                 "from the input data filename. Generates one plot for 5 min changes"
+                                                 "from the input data filename. Generates one plot for 5 min changes "
                                                  "and one for 60 min changes.")
     parser.add_argument("--data", type=str, required=True,
-                        help="This is the file with the q-values and log2 fold changes with the proteomic data.")
+                        help="The file with the q-values and log2 fold changes with the proteomic data.")
     # These should match the thresholds used when generating the data tables
     parser.add_argument("--qvalThresh", type=float, default=0.1,
-                        help="This is the q-value cutoff to use for significance.")
+                        help="The q-value cutoff to use for significance.")
     parser.add_argument("--foldThresh", type=float, default=1.5,
-                        help="This is the fold change cutoff to use for significance.")
+                        help="The fold change cutoff to use for significance.")
 
     # Get command line args
     args = parser.parse_args()
@@ -27,33 +28,43 @@ def main():
     rows = len(df)
     print('Read {rows} rows')
 
+    # The Boolean significance calls are not used so drop the column
     df = df.drop(columns='Significant')
 
-    # TODO iterate over time points
-    time = '5 min'
+    # Iterate over time points
+    for time in ['5', '60']:
+        # Select the first four columns and the 5 min columns
+        pattern = time + ' min'
+        time_df = df.iloc[:, 0:4].join(df.loc[:, df.columns.str.contains(pat=pattern)])
 
-    # Select the first four columns and the 5 min columns
-    df5 = df.iloc[:, 0:4].join(df.loc[:, df.columns.str.contains(pat=time)])
+        # Select exp 1 and 2 data and rename to remove experiment labels
+        exp1_df = time_df.iloc[:, 0:4].join(time_df.loc[:, time_df.columns.str.contains(pat='Exp1')])
+        exp1_df.columns = exp1_df.columns.str.replace(f'Exp1 {time} min ', '')
 
-    # Select exp 1 and 2 data and rename to remove experiment labels
-    exp1_df = df5.iloc[:, 0:4].join(df5.loc[:, df5.columns.str.contains(pat='Exp1')])
-    exp1_df.columns = exp1_df.columns.str.replace('Exp1 5 min ', '')
+        exp2_df = time_df.iloc[:, 0:4].join(time_df.loc[:, time_df.columns.str.contains(pat='Exp2')])
+        exp2_df.columns = exp2_df.columns.str.replace(f'Exp2 {time} min ', '')
 
-    exp2_df = df5.iloc[:, 0:4].join(df5.loc[:, df5.columns.str.contains(pat='Exp2')])
-    exp2_df.columns = exp2_df.columns.str.replace('Exp2 5 min ', '')
+        # Reshape the data frame so that the values from both replicates are stacked vertically
+        combined_df = pd.concat([exp1_df, exp2_df], ignore_index=True)
+        assert combined_df.shape == (rows * 2, 6)
 
-    combined_df = pd.concat([exp1_df, exp2_df], ignore_index=True)
-    assert combined_df.shape == (rows * 2, 6)
+        print(combined_df.columns)
+        print(combined_df.head())
+        print(combined_df)
 
-    print(combined_df.columns)
-    print(combined_df.head())
-    print(combined_df)
-    combined_df.to_csv('../Results/base_analysis/AllProteinDataCombined.csv')
+        # Create the output filename by adding the time point to the input data filename
+        # The .png and .svg file extension is added automatically
+        inpath = Path(args.data)
+        outpath = str(Path(inpath.parent, inpath.stem)) + time + 'min'
 
-    # Use to_label=[] to disable labels
-    volcano(combined_df, log2fc='log2 fold change', pvalue='q-value', symbol='Uniprot', to_label=[], pval_thresh=args.qvalThresh,
-            log2fc_thresh=np.log2(args.foldThresh), pvalue_label='q-value', alpha=0.5, linewidth=0.5, symmetric_x_axis=True,
-            colors=['black', 'lightgrey'])
+        # combined_df.to_csv('../Results/base_analysis/AllProteinDataCombined.csv')
+
+        # Use to_label=[] to disable labels
+        volcano(combined_df, log2fc='log2 fold change', pvalue='q-value', symbol='Uniprot', to_label=[],
+                pval_thresh=args.qvalThresh,
+                log2fc_thresh=np.log2(args.foldThresh), pvalue_label='q-value', alpha=0.5, linewidth=0.5,
+                symmetric_x_axis=True,
+                colors=['black', 'lightgrey'], save=outpath, legend=False)
 
 
 if __name__ == "__main__":
