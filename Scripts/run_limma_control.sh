19d8d2ee4e28217dd89651e97ad76e019aa81817
Anthony Gitter <gitter@biostat.wisc.edu>
2023-04-28 11:33:13 -0500

    Indicate script stages and add volcano script

--- Scripts/run_limma_control.sh
+++ Scripts/run_limma_control.sh
@@ -5,8 +5,16 @@ output_prefix=${2:-../Results/control_analysis/}   # Output prefix to add to all
 foldchange_threshold=${3:-1.5}   # Fold change threshold
 qvalue_threshold=${4:-0.1} # q-value threshold
 
+###############
+# First stage #
+###############
+
 #Format data and create enrichment inputs
 python compare_control_conditions.py ${input_prefix}protein_plex1_raw.csv ${input_prefix}protein_plex3_raw.csv ${input_prefix}protein_plex2_raw.csv  ${input_prefix}phospho_plex1_raw.csv ${input_prefix}phospho_plex3_raw.csv ${input_prefix}phospho_plex2_raw.csv --verbose --outName=${output_prefix} --foldThresh=${foldchange_threshold} --qvalThresh=${qvalue_threshold};
+
+################
+# Second stage #
+################
 
 ##Run limma
 #for i in `ls -d1 $PWD/${output_prefix}exp*`; do echo $i; Rscript limma_test.R $i; done;
